e322c7cf13535f11c315fd80f5e396e134b93476
Erika Bailon <erba0445@colorado.edu>
2019-10-13 18:36:40 -0600

    Add files via upload

---
+++ Increment 1/Assets/scripts/gameplay/Ball.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Movement of the ball
+/// </summary>
+public class Ball : MonoBehaviour
+{
+    //const float MoveUnitsPerSecond = 2;
+
+    //BoxCollider collider 
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start()
+	{
+        //Vector2 vecMov = new Vector2(Mathf.Cos(Mathf.PI ), Mathf.Sin(Mathf.PI ));
+        //GetComponent<Rigidbody2D>().AddForce(vecMov  * 10f, ForceMode2D.Impulse);
+
+
+        const float MinImpulseForce = 3f;
+        const float MaxImpulseForce = 6f;
+        //loat angle = Random.Range(0, 2 * Mathf.PI);
+        float angle = Random.Range(Mathf.PI / 4, 7 * Mathf.PI/4);
+        float angle2 = Random.Range(3 * Mathf.PI / 4, 5 * Mathf.PI / 4);
+
+        Vector2 rightDirection = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+        Vector2 leftDirection = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+
+        float magnitude = Random.Range(MinImpulseForce, MaxImpulseForce);
+
+        GetComponent<Rigidbody2D>().AddForce(rightDirection * magnitude, ForceMode2D.Impulse);
+        GetComponent<Rigidbody2D>().AddForce(leftDirection * magnitude, ForceMode2D.Impulse);
+
+
+    }
+
+
+    void OnBecameInvisible()
+    {
+        //this.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        //print(GetComponent<Rigidbody2D>().velocity.magnitude);
+      Destroy(gameObject);
+    }
+}
---
+++ Increment 1/Assets/scripts/gameplay/Paddle.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A paddle
+/// </summary>
+public class Paddle : MonoBehaviour
+{
+    //variable for the paddles
+    private Rigidbody2D rb2d;
+
+    //needed for spawning
+    [SerializeField]
+    ScreenSide sideOfScreen;
+
+    float colliderHalfWidth;
+    float colliderHalfHeight;
+    BoxCollider2D collider;
+
+
+    /// Use this for initialization
+    void Start()
+	{
+        rb2d = GetComponent<Rigidbody2D>();
+
+        //save collider dimension values 
+        collider = GetComponent<BoxCollider2D>(); 
+        Vector3 diff = collider.bounds.max - collider.bounds.min; 
+        colliderHalfWidth = diff.x / 2; 
+        colliderHalfHeight = diff.y / 2;
+
+    }
+
+	/// Update is called once per frame
+
+	void FixedUpdate()
+	{
+        //assigning the sides
+        float rightInput = Input.GetAxis("Vertical");
+        float leftInput = Input.GetAxis("Vertical2");
+
+         //move base on input
+        Vector2 position = transform.position;
+       //float verticalInput = Input.GetAxis("Vertical");
+
+        if(sideOfScreen == ScreenSide.Left && leftInput != 0)
+        {
+            position.y += leftInput * ConfigurationUtils.PaddleMoveUnitsPerSecond * Time.deltaTime;
+
+        }
+       
+        if (sideOfScreen == ScreenSide.Right && rightInput != 0)
+        {
+            position.y += rightInput * ConfigurationUtils.PaddleMoveUnitsPerSecond * Time.deltaTime;
+
+        }
+     
+        transform.position = position;
+    }
+	
+}
