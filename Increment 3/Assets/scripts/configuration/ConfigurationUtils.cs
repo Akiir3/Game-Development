5e4c969809fd306d1d709de1a664d32691701f0e
Erika Bailon <erba0445@colorado.edu>
2019-10-13 18:50:53 -0600

    Add files via upload

---
+++ Increment 3/Assets/scripts/configuration/ConfigurationUtils.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Provides access to configuration data
+/// </summary>
+public static class ConfigurationUtils
+{
+    #region Properties
+
+    /// <summary>
+    /// Gets the paddle move units per second
+    /// </summary>
+    public static float PaddleMoveUnitsPerSecond
+    {
+        get { return 10; }
+    }
+
+    /// <summary>
+    /// Gets the impulse force to apply to a ball
+    /// to get it moving
+    /// </summary>
+    public static float BallImpulseForce
+    {
+        get { return 5; }
+    }
+
+    /// <summary>
+    /// Gets how many seconds a ball stays alive
+    /// </summary>
+    public static float BallLifeSeconds
+    {
+        get { return 10; }
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Initializes the configuration utils
+    /// </summary>
+    public static void Initialize()
+    {
+
+    }
+
+    public static int BallPoints //Gonna use this one for the score points
+    {
+        get { return 1; }
+    }
+
+    public static int HitPoints  //This one for the hit points
+    {
+        get { return 1; }
+    }
+}
---
+++ Increment 3/Assets/scripts/gameplay/HUD.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    //bounce cout support 
+    [SerializeField]
+    Text Ltext;
+    static Text  LBouncerCounter;
+    int Lbounces = 0;
+
+    [SerializeField]
+    Text Rtext ;
+    static Text RBouncerCounter;
+    int Rbounces = 0;
+
+    // Score count cupport
+    [SerializeField]
+    Text score;
+    static Text Stext;
+    static float LScore = 0;
+    static float RScore = 0;
+
+    //for the text once game starts, as in the video
+    const string ScorePrefix = "Hits: ";
+    const string ScoreDash = " - ";
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //as in the video to change the text when the game starts
+
+        LBouncerCounter = Ltext.GetComponent<Text>();
+        LBouncerCounter.text = ScorePrefix + Lbounces.ToString();
+
+        RBouncerCounter = Rtext.GetComponent<Text>();
+        RBouncerCounter.text = ScorePrefix + Rbounces.ToString();
+
+        Stext = score.GetComponent<Text>();
+        Stext.text = LScore.ToString() + ScoreDash + RScore.ToString();
+
+    }
+
+    //functon to add the time the ball hits
+    public void AddPoints(int points, ScreenSide DefaultLeft)
+    {
+        if(DefaultLeft == ScreenSide.Left)
+        {
+            Lbounces += points;
+            LBouncerCounter.text = ScorePrefix + Lbounces.ToString();
+
+        }
+        else
+        {
+            Rbounces += points;
+            RBouncerCounter.text = ScorePrefix + Rbounces.ToString();
+        }
+
+    }
+
+    //functoin to add the times the ball gets out of the scene
+    public void AddToScore(int realPoints, ScreenSide scoreSide)
+    {
+        if(scoreSide == ScreenSide.Left)
+        {
+            LScore += realPoints;
+        }
+        else
+        {
+            RScore += realPoints;
+        }
+
+        Stext.text = LScore.ToString() + ScoreDash + RScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
